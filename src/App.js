5399dfbac36b649bb692210590c9b3c55fe2ebae
JoshuaBravo20 <69777229+JoshuaBravo20@users.noreply.github.com>
2021-01-25 12:45:55 -0300

    first-portfolio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./views/home";
+import NotFound from "./views/notfound";
+import Header from "./components/header";
 
 function App() {
+  const [state, setState] = useState({
+    user: null,
+  });
+
+  useEffect(() => {
+    getUser("https://api.github.com/users/JoshuaBravo20");
+  }, []);
+
+  /*  const getUser = (url) => {
+    fetch(url)
+      .then(resp => resp.json())
+      .then(data => {
+        setState({
+          ...state, user: data
+        })
+      })
+  } */
+
+  const getUser = async (url) => {
+    try {
+      const resp = await fetch(url);
+      const data = await resp.json();
+
+      setState({
+        ...state,
+        user: data,
+      });
+    }catch(error){
+      console.error(error.message);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Header user={state.user} />
+        <div className="main-wrapper">
+          <Switch>
+            <Route exact path="/" component={Home}></Route>
+            <Route component={NotFound}></Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
 
